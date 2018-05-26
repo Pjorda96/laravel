938c6afc8a06e7364210a9379aa229ea6039f42a
PABLOPC\PabloPortatil <pjorda96@gmail.com>
2018-05-26 12:24:27 +0200

    migraciones con llaves foraneas

--- cursoLaravel/database/migrations/2018_05_26_100306_add_profession_id_to_users.php
+++ cursoLaravel/database/migrations/2018_05_26_100306_add_profession_id_to_users.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddProfessionToUsers extends Migration
+class AddProfessionIdToUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,9 @@ class AddProfessionToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table){
-            $table->string('profession', 50)->nullable()->after('password');
+            $table->unsignedInteger('profession_id');
+            //$table->integer('profession_id')->unsigned();
+            $table->foreign('profession_id')->references('id')->on('professions');
         });
     }
 
@@ -25,8 +27,10 @@ class AddProfessionToUsers extends Migration
      */
     public function down()
     {
+//si solo se usa art migrate:fresh no hace falta el drop
         Schema::table('users', function (Blueprint $table){
-            $table->dropColumn('profession');
+            $table->dropForeign(['profession_id']);
+            $table->dropColumn('profession_id');
         });
     }
 }
